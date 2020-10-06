b5ad3d2cb4abf3aa2eef1cdce2a332f7462bba3f
Emma Tysk <etysk@uber.com>
2020-10-06 16:06:40 +0300

    importing prism to the vue component to not have to use markdown language

--- src/.vuepress/components/graph-sample.vue
+++ src/.vuepress/components/graph-sample.vue
@@ -1,7 +1,70 @@
 <template>
   <div class="graph-container">
     <div class="graph-code">
-      <Content slot-key="graph-code" />
+      <pre><code class="language-java">public class HelloActivity {
+
+  static final String TASK_LIST = "HelloActivity";
+
+  /** Workflow interface has to have at least one method annotated with @WorkflowMethod. */
+  public interface GreetingWorkflow {
+    /** @return greeting string */
+    @WorkflowMethod(executionStartToCloseTimeoutSeconds = 10, taskList = TASK_LIST)
+    String getGreeting(String name);
+  }
+
+  /** Activity interface is just a POJI. */
+  public interface GreetingActivities {
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 2)
+    String composeGreeting(String greeting, String name);
+  }
+
+  /** GreetingWorkflow implementation that calls GreetingsActivities#composeGreeting. */
+  public static class GreetingWorkflowImpl implements GreetingWorkflow {
+
+    /**
+     * Activity stub implements activity interface and proxies calls to it to Cadence activity
+     * invocations. Because activities are reentrant, only a single stub can be used for multiple
+     * activity invocations.
+     */
+    private final GreetingActivities activities =
+        Workflow.newActivityStub(GreetingActivities.class);
+
+    @Override
+    public String getGreeting(String name) {
+      // This is a blocking call that returns only after the activity has completed.
+      return activities.composeGreeting("Hello", name);
+    }
+  }
+
+  static class GreetingActivitiesImpl implements GreetingActivities {
+    @Override
+    public String composeGreeting(String greeting, String name) {
+      return greeting + " " + name + "!";
+    }
+  }
+
+  public static void main(String[] args) {
+    // Start a worker that hosts both workflow and activity implementations.
+    Worker.Factory factory = new Worker.Factory(DOMAIN);
+    Worker worker = factory.newWorker(TASK_LIST);
+    // Workflows are stateful. So you need a type to create instances.
+    worker.registerWorkflowImplementationTypes(GreetingWorkflowImpl.class);
+    // Activities are stateless and thread safe. So a shared instance is used.
+    worker.registerActivitiesImplementations(new GreetingActivitiesImpl());
+    // Start listening to the workflow and activity task lists.
+    factory.start();
+
+    // Start a workflow execution. Usually this is done from another program.
+    WorkflowClient workflowClient = WorkflowClient.newInstance(DOMAIN);
+    // Get a workflow stub using the same task list the worker uses.
+    GreetingWorkflow workflow = workflowClient.newWorkflowStub(GreetingWorkflow.class);
+    // Execute a workflow waiting for it to complete.
+    String greeting = workflow.getGreeting("World");
+    System.out.println(greeting);
+    System.exit(0);
+  }
+}</code>
+</pre>
     </div>
     <div class="graph">
       <iframe
@@ -15,17 +78,24 @@
   </div>
 </template>
 <script>
+import "prismjs";
+import "prismjs/components/prism-java";
 export default {
   name: "graph-sample",
+  mounted() {
+    Prism.highlightAll();
+  },
 };
 </script>
 <style lang="styl" scoped>
 .graph-code {
   height: 100%;
   overflow-y: scroll;
+  border-radius: 6px;
 }
 
 .graph-container {
+  margin: 0.85rem;
   display: flex;
   width: 100%;
   height: 800px;
@@ -36,6 +106,6 @@ export default {
 }
 .graph-code {
   flex: 1;
+  height: 100%;
 }
 </style>
-
